376ec5883fd175363ec8d114149efbb28f50f6d1
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-19 00:00:16 +0300

    added file travel_mate_state.dart to cubit folder

---
+++ lib/features/home/presentation/cubit/travel_mate_state.dart
@@ -0,0 +1,29 @@
+import 'package:travel_mate_ui/config/constants/categories.dart';
+import 'package:travel_mate_ui/config/constants/hotel.dart';
+import 'package:travel_mate_ui/config/constants/popular_place.dart';
+
+sealed class TravelMateState {}
+
+class TravelMateInitial extends TravelMateState {}
+
+class CategoriesLoading extends TravelMateState {}
+
+class CategoriesSuccess extends TravelMateState {
+  final List<Categories> listCategories;
+
+  CategoriesSuccess({required this.listCategories});
+}
+
+class HotelSuccess extends TravelMateState {
+  final List<Hotel> listHotel;
+
+  HotelSuccess({required this.listHotel});
+}
+
+class GetPopularPlace extends TravelMateState {
+  final List<PopularPlace> listPopularPlace;
+
+  GetPopularPlace({required this.listPopularPlace});
+}
+
+class OnTapCategory extends TravelMateState {}
