41a13ffb62cc7cc633a990f4e9418ba43d9fa8a3
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-18 23:48:41 +0300

    added file list_popular_place.dart to constants folder

---
+++ lib/config/constants/list_popular_place.dart
@@ -0,0 +1,34 @@
+import 'package:travel_mate_ui/config/constants/popular_place.dart';
+
+class ListPopularPlace {
+  List<PopularPlace> listpopularPlace = [
+    PopularPlace(
+      description:
+          "An intrinsic part of the Cyclades Islands in the Aegean Sea, the Greek Island of Mykonos is all about age-old churches with stony white-washed façades, historical museums, exotic beaches, exciting bars and restaurants, dance clubs and luxury cruises.",
+      image:
+          "https://hblimg.mmtcdn.com/content/hubble/img/schengenvideoimg/mmt/activities/m_mykonos_7_l_643_1000.jpg",
+      title: "Mykonos",
+    ),
+    PopularPlace(
+      description:
+          "Nestled on the banks of River Beas, Manali will take your breath away with its snow-kissed peaks, lush valleys and heart-pumping adventure experiences in the heart of Himalayas.",
+      image:
+          "https://hblimg.mmtcdn.com/content/hubble/img/destimg/mmt/activities/m_Manali_destjulimg_1_l_799_1418.jpg",
+      title: "Manali",
+    ),
+    PopularPlace(
+      description:
+          "Flaunting one of the best cliff beaches in Kerala, Varkala is a lesser known yet absolutely paradisiacal destination that houses forts, pilgrimage sites, springs, natural fisheries and more.",
+      image:
+          "https://hblimg.mmtcdn.com/content/hubble/img/varkala/mmt/destination/m_destination_Varkala_landscape_l_400_640.jpg",
+      title: "Varkala",
+    ),
+    PopularPlace(
+      description:
+          "The second most populous city in Netherlands, Rotterdam has risen like a phoenix after the WWII mass destruction it witnessed. Today it is know for its amazing reconstruction through innovative urban designs, beautiful inter-city canals, charming bridges, Dutch delicacies, a great café culture and a buzzing nightlife.",
+      image:
+          "https://hblimg.mmtcdn.com/content/hubble/img/int_dest/mmt/activities/m_rotterdam_2_l_600_900.jpg",
+      title: "Rotterdam",
+    ),
+  ];
+}
