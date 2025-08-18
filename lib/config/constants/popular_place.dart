9134e8e9d18dd02046fbb68e2b22b0db62db694d
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-18 23:48:59 +0300

    added file popular_place.dart to constants folder

---
+++ lib/config/constants/popular_place.dart
@@ -0,0 +1,8 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+
+class PopularPlace {
+  String? image;
+  String? description;
+  String? title;
+  PopularPlace({this.description, this.image, this.title});
+}
