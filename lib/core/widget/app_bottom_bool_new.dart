51d6f747ca980b358fedb17660181816daff19a1
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-21 18:22:56 +0300

    added app_bottom_bool_new to widget folder

--- lib/core/widget/app_bottom_bool_new.dart
+++ lib/core/widget/app_bottom_bool_new.dart
@@ -8,6 +8,7 @@ class AppBottomBoolNew extends StatelessWidget {
   final double? sigmaY;
   final String? title;
   final TextStyle? style;
+  final void Function()? onTap;
   const AppBottomBoolNew({
     super.key,
     required this.width,
@@ -16,49 +17,53 @@ class AppBottomBoolNew extends StatelessWidget {
     this.sigmaY,
     this.title,
     this.style,
+    this.onTap,
   });
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadiusGeometry.circular(30),
-      child: SizedBox(
-        width: width,
-        height: height,
-        child: Stack(
-          children: [
-            BackdropFilter(
-              filter: ImageFilter.blur(
-                sigmaX: sigmaX ?? 3,
-                sigmaY: sigmaY ?? 3,
-              ),
-              child: SizedBox(
-                child: Center(
-                  child: Text(
-                    title ?? "Book New",
-                    style: style ?? TextStyle(color: Colors.white),
+    return GestureDetector(
+      onTap: onTap,
+      child: ClipRRect(
+        borderRadius: BorderRadiusGeometry.circular(30),
+        child: SizedBox(
+          width: width,
+          height: height,
+          child: Stack(
+            children: [
+              BackdropFilter(
+                filter: ImageFilter.blur(
+                  sigmaX: sigmaX ?? 3,
+                  sigmaY: sigmaY ?? 3,
+                ),
+                child: SizedBox(
+                  child: Center(
+                    child: Text(
+                      title ?? "Book New",
+                      style: style ?? TextStyle(color: Colors.white),
+                    ),
                   ),
                 ),
               ),
-            ),
-            Container(
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.white.withOpacity(0.6),
-                  width: 0.5,
-                ),
-                borderRadius: BorderRadius.circular(30),
-                gradient: LinearGradient(
-                  begin: Alignment.bottomLeft,
-                  end: Alignment.bottomRight,
-                  colors: [
-                    Colors.white.withOpacity(0.1),
-                    Colors.white.withOpacity(0.1),
-                  ],
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white.withOpacity(0.6),
+                    width: 0.5,
+                  ),
+                  borderRadius: BorderRadius.circular(30),
+                  gradient: LinearGradient(
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.bottomRight,
+                    colors: [
+                      Colors.white.withOpacity(0.1),
+                      Colors.white.withOpacity(0.1),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
