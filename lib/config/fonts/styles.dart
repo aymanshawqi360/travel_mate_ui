cfd32782518fb35243dbd82100e8c5afd5abfcde
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-21 18:16:08 +0300

    added styles in fonts folder

--- lib/config/fonts/styles.dart
+++ lib/config/fonts/styles.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:travel_mate_ui/config/fonts/colors_manager.dart';
 import 'package:travel_mate_ui/config/fonts/font_weight.dart';
+import 'package:travel_mate_ui/core/utils/hex_color.dart';
 
 class TextStyles {
   TextStyles._();
@@ -13,5 +14,26 @@ class TextStyles {
     color: ColorsManager.black,
     fontWeight: FontWeightHelper.medium,
     fontSize: 17,
+  );
+  static final TextStyle font15Medium = TextStyle(
+    color: HexColor(hexColor: '7E8186'),
+    fontWeight: FontWeightHelper.medium,
+    fontSize: 15,
+  );
+  static final TextStyle font16Medium = TextStyle(
+    color: HexColor(hexColor: "FFFFFF"),
+    fontWeight: FontWeightHelper.medium,
+    fontSize: 16,
+  );
+  static final TextStyle font13Light = TextStyle(
+    color: HexColor(hexColor: "7E8186"),
+    fontWeight: FontWeightHelper.light,
+    fontSize: 13,
+  );
+  static final TextStyle font23Bold = TextStyle(
+    color: ColorsManager.black,
+    fontWeight: FontWeightHelper.bold,
+    fontSize: 23,
+    decoration: TextDecoration.underline,
   );
 }
