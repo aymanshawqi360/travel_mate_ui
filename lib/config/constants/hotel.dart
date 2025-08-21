9fcc0c4a00519e1ed17d97bf33abb8c9f47d8868
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-21 18:14:01 +0300

    added class hotel to constants folder

--- lib/config/constants/hotel.dart
+++ lib/config/constants/hotel.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
 
 class Hotel {
+  int? id;
   String? image;
   String? title;
   String? hotelFeatures;
-  List<IconData>? icon;
+  List<Map<String, IconData>>? icon;
   String? description;
   String? address;
+  int? price;
 
   Hotel({
+    this.id,
     this.image,
+    this.price,
     this.title,
     this.hotelFeatures,
     this.description,
