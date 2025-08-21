a45a5bc796f47712baab320d606468b165fefe8f
aymanshawqi360 <aymanshawqi380gmail.com>
2025-08-21 18:20:19 +0300

    added app_strings to utils folder

--- lib/core/utils/app_strings.dart
+++ lib/core/utils/app_strings.dart
@@ -2,4 +2,9 @@ class AppStrings {
   AppStrings._();
   static const String nearbyDestination = "Nearby Destination";
   static const String popularPlace = "Popular place";
+  static const String price = "Price";
+  static const String bookNow = "Book Now";
+  static const String description = "Description";
+  static const String night = " /Night";
+  static const String whatThisPlaceOffers = "What this place offers";
 }
